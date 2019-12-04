0251ee3db4b59b683c1094c948d9bc36f12d70e1
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-12-04 10:04:12 -0300

    Class User

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ var user = {};
 
 function add(dataUser) {
 
+    console.log(dataUser);
+
     document.getElementById("table-users").innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
@@ -37,6 +39,15 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
     });
 
-    add(user);
+    var objectUser = new User(user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin);
+
+    add(objectUser);
 
 });

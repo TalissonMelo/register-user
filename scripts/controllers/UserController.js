1ecc17e3ea64b472a20ed1acdd0b98e9508604af
TalissonMelo <talissonmelorodrigues@gmail.com>
2019-12-29 14:26:29 -0300

    Updating User Statistics

--- scripts/controllers/UserController.js
+++ scripts/controllers/UserController.js
@@ -124,6 +124,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -138,6 +140,27 @@ class UserController {
 
         this.tableEl.appendChild(tr);
 
+        this.updateCount();
+
     }
 
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) {
+                numberAdmin++;
+            }
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+    }
 }
